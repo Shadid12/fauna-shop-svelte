fb9d7612a9151503dc83a2a98ffd0c262641b240
shaque <shaque@omers.com>
2021-12-17 19:52:05 -0500

    fixed added initial repo

---
+++ README.md
@@ -0,0 +1,15 @@
+### Create a new app
+
+```sh
+npm init svelte@next fauna-shop
+cd fauna-shop
+npm i 
+npm run dev -- --open
+```
+
+### Integrate Apollo Client to Svelt
+
+```sh
+npm i @apollo/client svelte-apollo graphql --save
+```
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { query } from "svelte-apollo";
+  import { gql } from "@apollo/client/core";
+  const GET_POSTS = gql`
+    {
+    listPosts {
+      data {
+        id
+        title
+        author {
+          _id
+          name
+        }
+        tags
+      }
+      after
+      before
+    }
+  }
+  `;
+
+  const posts = query(GET_POSTS);
+  console.log(posts);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+{#await $posts}
+  Loading...
+{:then result}
+{JSON.stringify(result)}
+{:catch error}
+{error}
+{/await}
